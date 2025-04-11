c4cf3d38b917e7f1cc7d541a29fe7b7b60a1c6de
gondalpreeti <gondalpreeti@gmail.com>
2025-04-11 20:56:10 +0530

    Delete resume_assistant directory

--- resume_assistant/utils/bias_detector.py
+++
@@ -1,7 +0,0 @@
-def detect_bias(text):
-    alerts = []
-    if "he/she" in text.lower():
-        alerts.append("Avoid using gendered language like 'he/she'.")
-    if "aggressive" in text.lower():
-        alerts.append("Bias alert: Words like 'aggressive' can imply gender bias.")
-    return alerts
